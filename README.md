c47e437e5031f93796556e92e579139eb68bef00
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-10-21 16:07:43 +0200

    Add initial version

---
+++ README.md
@@ -0,0 +1,87 @@
+# Utilities for coding with Julia arrays
+
+This package provides a number of methods and types to deal with the variety of
+array types (sub-types of `AbstractArray`) that exist in Julia.
+
+These are useful to implement methods to process arrays in a generic way.
+
+## Provided tools
+
+### Array indexing
+
+The `eachindex` method is very useful When writing loops over array elements so
+as to be agnostic to which specfic indexing rule is the most suitable.  Some
+algorithms are however more efficient or easier to write if all involved arrays
+are indexed by a single 1-based index.  In that case,
+
+```julia
+fastarray(A)
+```
+
+checks whether array `A` is suitable for fast indexing (by a single integer
+starting at 1); if it does, `A` is returned to the caller; otherwise, the
+contents of `A` is converted to a suitable array type implementing fast
+indexing and is returned to the caller.
+
+
+To just check whether array `A` is suitable for fast indexing, call:
+
+```julia
+isfastarray(A) -> bool
+```
+
+Multiple arguments can be checked at the same time: `isfastarray(A,B,...)` is
+the same as `isfastarray(A) && isfastarray(B) && isflatarray(...)`.
+
+
+### Array storage
+
+When calling (with `ccall`) a compiled function coded in another language (C,
+FORTRAN, etc.), you have to make sure that array arguments have the same
+storage as assumed by these languages so that it is safe to pass the pointer of
+the array to the compiled function.
+
+Typically, you want to ensure that the elements are stored in memory
+contiguously and in column-major order.  This can be ckecked by calling:
+
+```julia
+isflatarray(A) -> bool
+```
+
+or, with several arguments:
+
+```julia
+isflatarray(A, B, C, ...)  -> bool
+```
+
+In order to get an array with such *flat* storage and possibly with a given
+element type `T`, call:
+
+```julia
+flatarray([T = eltype(A),] A)
+```
+
+which just returns `A` if the requirements hold or converts `A` to a suitable
+array form.
+
+
+## FAQ
+
+* What is the difference between `IndexStyle` (defined in base Julia) and
+  `IndexingTrait` (defined in this package)?
+
+  If `IndexStyle(A) === IndexLinear()`, then array `A` can be efficiently
+  indexed by one integer (even if `A` is multidimensional) and column-major
+  ordering is used to access the elements of `A`.  The only (known) other
+  possibility is `IndexStyle(A) === IndexCartesian()`.
+
+  If `IndexingTrait(A) === FastIndexing()`, then `IndexStyle(A) ===
+  IndexLinear()` also holds (see above) **and** array `A` has standard 1-based
+  indices.
+
+* What is the difference between `Base.has_offset_axes` (provided by Julia) and
+  `has_standard_indexing` (provided by this package)?
+
+  For the caller, `has_standard_indexing(args...)` yields the opposite result
+  as `Base.has_offset_axes(args...)`.  Furthermore, `has_standard_indexing` is
+  a bit faster.
---
+++ src/ArrayTools.jl
@@ -0,0 +1,551 @@
+module ArrayTools
+
+export
+    allof,
+    anyof,
+    checkdimensions,
+    colons,
+    dimensions,
+    has_standard_indexing,
+    indices,
+    noneof,
+    reversemap,
+    # storage trait
+    StorageType,
+    AnyStorage,
+    FlatStorage,
+    flatarray,
+    flatvector,
+    flatmatrix,
+    isflatarray,
+    # Fast arrays and indexing trait:
+    IndexingTrait,
+    FastIndexing,
+    AnyIndexing,
+    fastarray,
+    isfastarray
+
+"""
+```julia
+StorageType(A)
+```
+
+yields the type of storage of the elements of argument `A`.  If `A` is a *flat*
+array, that is an array with contiguous elements in column-major order and
+first element at index 1, the singleton `FlatStorage()` is returned; otherwise,
+the singleton `AnyStorage()` is returned.
+
+This method can be extended for custom array types to quickly return the
+correct answer.
+
+See also [`isflatarray`](@ref), [`flatarray`](@ref).
+
+"""
+abstract type StorageType end
+
+struct FlatStorage <: StorageType end
+struct  AnyStorage <: StorageType end
+
+@doc @doc(StorageType) FlatStorage
+@doc @doc(StorageType)  AnyStorage
+
+StorageType(::Array) = FlatStorage()
+StorageType(::Any) = AnyStorage()
+StorageType() = AnyStorage()
+function StorageType(A::StridedVector)::StorageType
+    (first(axes(A,1)) == 1 && stride(A,1) == 1) ? FlatStorage() : AnyStorage()
+end
+function StorageType(A::StridedMatrix)::StorageType
+    inds, dims, stds = axes(A), size(A), strides(A)
+    (first(inds[1]) == 1 && stds[1] == 1 &&
+     first(inds[2]) == 1 && stds[2] == dims[1]) ?
+     FlatStorage() : AnyStorage()
+end
+function StorageType(A::StridedArray{T,3})::StorageType where {T}
+    inds, dims, stds = axes(A), size(A), strides(A)
+    (first(inds[1]) == 1 && stds[1] == 1 &&
+     first(inds[2]) == 1 && stds[2] == dims[1] &&
+     first(inds[3]) == 1 && stds[3] == dims[1]*dims[2]) ?
+     FlatStorage() : AnyStorage()
+end
+function StorageType(A::StridedArray{T,4})::StorageType where {T}
+    inds, dims, stds = axes(A), size(A), strides(A)
+    (first(inds[1]) == 1 && stds[1] == 1 &&
+     first(inds[2]) == 1 && stds[2] == dims[1] &&
+     first(inds[3]) == 1 && stds[3] == dims[1]*dims[2] &&
+     first(inds[4]) == 1 && stds[4] == dims[1]*dims[2]*dims[3]) ?
+     FlatStorage() : AnyStorage()
+end
+function StorageType(A::StridedArray{T,N})::StorageType where {T,N}
+    inds, dims, stds = axes(A), size(A), strides(A)
+    n = 1
+    @inbounds for d in 1:N
+        if first(inds[d]) != 1 || stds[d] != n
+            return AnyStorage()
+        end
+        n *= dims[d]
+    end
+    return FlatStorage()
+end
+
+
+"""
+```julia
+isflatarray(A) -> boolean
+```
+
+yields whether array `A` can be indexed as a *flat* array, that is an array
+with contiguous elements in column-major order and first element at index 1.
+This also means that `A` has 1-based indices along all its dimensions.
+
+Several arguments can be checked in a single call:
+
+```julia
+isflatarray(A, B, C, ...)
+```
+
+is the same as:
+
+```julia
+isflatarray(A) && isflatarray(B) && isflatarray(C) && ...
+```
+
+See also [`StorageType`](@ref), [`flatarray`](@ref),
+[`has_standard_indexing`](@ref).
+
+"""
+isflatarray() = false
+isflatarray(args...) = allof(isflatarray, args...)
+isflatarray(A::AbstractArray) = _isflatarray(StorageType(A))
+_isflatarray(::FlatStorage) = true
+_isflatarray(::StorageType) = false
+#
+# `isflatarray(args...)` could have been:
+#
+#     isflatarray(args...) = all(isflatarray, args)
+#
+# but using `all(isflatarray, A, x, y)` for `A`, `x` and `y` flat arrays of
+# sizes (3,4,5,6), (5,6) and (3,4) takes 9.0ns with Julia 1.0 (29.1ns with
+# Julia 0.6) while using `allof` takes 0.02ns (i.e. is eliminated by the
+# compiler).
+#
+
+"""
+```julia
+flatarray([T=eltype(A),] A)
+```
+
+lazily yields a *flat* array based on `A`, that is an array with contiguous
+elements in column-major order and first element at index 1.  Optional argument
+`T` is to specify the element type of the result.  Argument `A` is returned if
+it is already a flat array with the requested element type; otherwise,
+[`convert`](@ref) is called to produce the result (an `Array{T}` in that case).
+
+Similarly:
+
+```julia
+flatvector([T=eltype(V),] V)
+flatmatrix([T=eltype(M),] M)
+```
+
+respectively yield a *flat* vector from `V` and a *flat* matrix from `M`.
+
+See also [`isflatarray`](@ref), [`convert`](@ref).
+"""
+flatarray(A::Array) = A
+flatarray(::Type{T}, A::Array{T,N}) where {T,N} = A
+flatarray(A::AbstractArray{T,N}) where {T,N} =
+    _flatarray(StorageType(A), T, A)
+flatarray(::Type{T}, A::AbstractArray{<:Any,N}) where {T,N} =
+    _flatarray(StorageType(A), T, A)
+
+_flatarray(::FlatStorage, ::Type{T}, A::AbstractArray{T,N}) where {T,N} = A
+_flatarray(::StorageType, ::Type{T}, A::AbstractArray{<:Any,N}) where {T,N} =
+    convert(Array{T,N}, A)
+
+flatvector(V::AbstractVector{T}) where {T} = flatarray(T, V)
+flatvector(::Type{T}, V::AbstractVector) where {T} = flatarray(T, V)
+@doc @doc(flatarray) flatvector
+
+flatmatrix(M::AbstractMatrix{T}) where {T} = flatarray(T, M)
+flatmatrix(::Type{T}, M::AbstractMatrix) where {T} = flatarray(T, M)
+@doc @doc(flatarray) flatmatrix
+
+"""
+
+```julia
+IndexingTrait(A)
+```
+
+yields one of the singletons `FastIndexing()` or `AnyIndexing()` to indicate
+whether or not array `A` has standard 1-based indices and can be efficiently
+indexed by one integer (even if `A` is multidimensional) and column-major
+ordering is used to access the elements of `A`.
+
+This method can be extended for custom array types to quickly return the
+correct answer.
+
+See also: [`isfastarray`](@ref), [`fastarray`](@ref).
+
+"""
+abstract type IndexingTrait end
+
+struct FastIndexing <: IndexingTrait end
+struct  AnyIndexing <: IndexingTrait end
+
+@doc @doc(IndexingTrait) FastIndexing
+@doc @doc(IndexingTrait) AnyIndexing
+
+IndexingTrait(::Array) = FastIndexing()
+IndexingTrait(::Any) = AnyIndexing()
+IndexingTrait(A::AbstractArray) =
+    (IndexStyle(A) === IndexLinear() && has_standard_indexing(A) ?
+     FastIndexing() : AnyIndexing())
+
+"""
+
+```julia
+isfastarray(A)
+```
+
+yields whether array `A` has standard 1-based indices and is efficiently
+indexed by linear indices.
+
+Several arguments can be checked in a single call:
+
+```julia
+isfastarray(A, B, C, ...)
+```
+
+is the same as:
+
+```julia
+isfastarray(A) && isfastarray(B) && isfastarray(C) && ...
+```
+
+See also: [`IndexingType`](@ref), [`fastarray`](@ref).
+
+"""
+isfastarray() = false
+isfastarray(args...) = allof(isfastarray, args...)
+isfastarray(A::AbstractArray) = _isfastarray(IndexingTrait(A))
+_isfastarray(::FastIndexing) = true
+_isfastarray(::AnyIndexing) = false
+
+"""
+
+```julia
+fastarray([T=eltype(A),] A)
+```
+
+yields a fast array equivalent to `A` with element type `T`.  If `A` is already
+a fast array with element type `T`, A is returned; otherwise, `A` is converted
+into an `Array` which is returned.
+
+See also: [`isfastarray`](@ref), [`IndexingTrait`](@ref).
+
+"""
+fastarray(A::AbstractArray{T,N}) where {T,N} = fastarray(T, A)
+fastarray(::Type{T}, A::AbstractArray{<:Any,N}) where {T,N} =
+    _fastarray(IndexingTrait(A), T, A)
+_fastarray(::FastIndexing, ::Type{T}, A::AbstractArray{T,N}) where {T,N} = A
+_fastarray(::IndexingTrait, ::Type{T}, A::AbstractArray{<:Any,N}) where {T,N} =
+    convert(Array{T,N}, A)
+
+"""
+
+```julia
+dimensions(arg) -> dims
+```
+
+yields a list of dimensions (as a tuple of `Int`) out of argument `arg`.  The
+union `Dimensions` matches the types of argument `arg` acceptable for
+`dimensions(arg)`: scalar integer and tuple of integers.
+
+As a special case, for an array `A`:
+
+```julia
+dimensions(A) -> size(A)
+```
+
+yields the list of dimensions of `A`, that is `size(A)`, after having checked
+that `A` has standard 1-based indices.
+
+See also: [`has_standard_indexing`].
+
+"""
+dimensions(dims::Tuple{}) = dims
+dimensions(dim::Integer) = (Int(dim),)
+dimensions(dims::Tuple{Vararg{Integer}}) = map(Int, dims)
+dimensions(dims::Integer...) = map(Int, dims)
+dimensions(dim::Int) = (dim,)
+dimensions(dims::Tuple{Vararg{Int}}) = dims
+dimensions(dims::Int...) = dims
+
+function dimensions(A::AbstractArray)
+    has_standard_indexing(A) || throw_non_standard_indexing()
+    return size(A)
+end
+
+const Dimensions = Union{Integer,Tuple{Vararg{Integer}}}
+
+@doc @doc(dimensions) Dimensions
+
+@noinline throw_non_standard_indexing() =
+    error("array have non-standard indices")
+
+"""
+
+```julia
+checkdimensions(dims)
+```
+
+checks whether `dims` is a list of valid dimensions.  An error is thrown if not
+all dimensions are nonnegative.
+
+See also: [`dimensions`](@ref).
+
+"""
+checkdimensions(::Tuple{}) = true
+checkdimensions(dims::Tuple{Vararg{Integer}}) =
+    allof(d -> d ≥ 0, dims...) || error("invalid array dimension(s)")
+checkdimensions(dim::Integer) =
+    dim ≥ 0 || error("invalid array dimension")
+
+
+"""
+
+```julia
+colons(n)
+colons(Val(n))
+```
+
+yields a tuple of `n` colons `:`.  The second call is faster when `n` is known
+at compile time.
+
+This method is suitable to extract sub-arrays of build views when some kind of
+rubber index is needed.  For instance:
+
+```julia
+slice(A::AbstractArray{T,N}, i::Integer) where {T,N} =
+    A[colons(Val(N-1))..., i]
+```
+
+defines a function that returns the `i`-th slice of `A` assuming index `i`
+refers the last index of `A`.
+
+"""
+colons(n::Integer) =
+    (n ==  0 ? () :
+     n ==  1 ? (:,) :
+     n ==  2 ? (:,:,) :
+     n ==  3 ? (:,:,:,) :
+     n ==  4 ? (:,:,:,:,) :
+     n ==  5 ? (:,:,:,:,:,) :
+     n ==  6 ? (:,:,:,:,:,:,) :
+     n ==  7 ? (:,:,:,:,:,:,:,) :
+     n ==  8 ? (:,:,:,:,:,:,:,:,) :
+     n ==  9 ? (:,:,:,:,:,:,:,:,:,) :
+     n == 10 ? (:,:,:,:,:,:,:,:,:,:,) :
+     _colons(n))
+
+function _colons(n::Integer)
+    n ≥ 0 || throw(ArgumentError(string("number of dimensions should be ≥ 0, got ", n)))
+    return ([Colon() for i in 1:n]...,)
+end
+
+colons(::Val{ 0}) = ()
+colons(::Val{ 1}) = (:,)
+colons(::Val{ 2}) = (:,:,)
+colons(::Val{ 3}) = (:,:,:,)
+colons(::Val{ 4}) = (:,:,:,:,)
+colons(::Val{ 5}) = (:,:,:,:,:,)
+colons(::Val{ 6}) = (:,:,:,:,:,:,)
+colons(::Val{ 7}) = (:,:,:,:,:,:,:,)
+colons(::Val{ 8}) = (:,:,:,:,:,:,:,:,)
+colons(::Val{ 9}) = (:,:,:,:,:,:,:,:,:,)
+colons(::Val{10}) = (:,:,:,:,:,:,:,:,:,:,)
+colons(v::Val{N}) where {N} = ntuple(x -> :, v)
+
+"""
+
+```julia
+has_standard_indexing(A)
+```
+
+return `true` if the indices of `A` start with 1 along all axes.  Can be called
+with multiple arguments:
+
+```julia
+has_standard_indexing(A, B, ...)
+```
+
+is equivalent to:
+
+```julia
+has_standard_indexing(A) && has_standard_indexing(B) && ...
+```
+
+Opposite of `Base.has_offset_axes` which is not available in version of Julia
+older than 0.7.
+
+"""
+has_standard_indexing(arg) = allof(x -> first(x) == 1, axes(arg)...)
+has_standard_indexing(args...) = allof(has_standard_indexing, args...)
+has_standard_indexing(arg::Array) = true
+
+"""
+
+The calls:
+
+```julia
+indices(A)
+indices((n1, n2, ...))
+indices((i1:j1, i2:j2, ...))
+indices(CartesianIndex(i1, i2, ...), CartesianIndex(j1, j2, ...))
+indices(R)
+```
+
+all yield an instance of `CartesianIndices` suitable for multi-dimensional
+indexing of respectively: all the indices of array `A`, a multi-dimensional
+array of dimensions `(n1,n2,...)`, a multi-dimensional region whose first and
+last indices are `(i1,i2,...)` and `(j1,j2,...)` or a Cartesian region defined
+by `R`, an instance of `CartesianIndices`.
+
+If a single argument is provided that is an integer or a range (not a tuple),
+an `AbstractUnitRange` is returned.
+
+"""
+indices(A::AbstractArray) = indices(axes(A))
+indices(dim::Int) = Base.OneTo(dim)
+indices(dim::Integer) = Base.OneTo(Int(dim))
+indices(rng::AbstractUnitRange{Int}) = rng
+indices(rng::AbstractUnitRange{<:Integer}) = convert(UnitRange{Int}, rng)
+indices(R::CartesianIndices) = R
+indices(start::CartesianIndex{N}, stop::CartesianIndex{N}) where {N} =
+    CartesianIndices(map((i,j) -> i:j, start.I, stop.I))
+indices(dims::Tuple{Vararg{Integer}}) =
+    CartesianIndices(map(indices, dims))
+indices(rngs::NTuple{N,AbstractUnitRange{<:Integer}}) where {N} =
+    CartesianIndices(rngs)
+
+"""
+```julia
+allof(p, args...) -> Bool
+```
+
+checks whether predicate `p` returns `true` for all arguments in `args...`,
+returning `false` as soon as possible (short-circuiting).
+
+```julia
+allof(args...) -> Bool
+```
+
+checks whether all arguments `args...` are `true`, returning `false` as soon as
+possible (short-circuiting).  Arguments can be booleans or arrays of booleans.
+The latter are considered as `true` if all their elements are `true` and are
+considered as `false` otherwise (if any of their elements are `false`).
+Arguments can also be iterables to check whether all their values are `true`.
+An empty iterable is considered as `true`.
+
+This method can be much faster than `all(p, args)` or `all(args)` because its
+result may be determined at compile time.  However, `missing` values are not
+considered as special.
+
+See also: [`all`](@ref), [`anyof`](@ref), [`noneof`](@ref).
+
+"""
+allof(p::Function, a) = p(a)::Bool
+allof(p::Function, a, b...) = p(a) && allof(p, b...)
+allof(a, b...) = allof(a) && allof(b...)
+allof(a::Bool) = a
+function allof(a::AbstractArray{Bool})
+    @inbounds for i in eachindex(a)
+        a[i] || return false
+    end
+    return true
+end
+function allof(itr)
+    for val in itr
+        allof(val) || return false
+    end
+    return true
+end
+
+"""
+```julia
+anyof(p, args...) -> Bool
+```
+
+checks whether predicate `p` returns `true` for any argument `args...`,
+returning `true` as soon as possible (short-circuiting).
+
+```julia
+anyof(args...) -> Bool
+```
+
+checks whether all arguments `args...` are `true`, returning `false` as soon as
+possible (short-circuiting).  Arguments can be booleans or arrays of booleans.
+The latter are considered as `true` if any of their elements are `true` and are
+considered as `false` otherwise (if all their elements are `false`).  Arguments
+can also be iterables to check whether any of their values are `true`.  An
+empty iterable is considered as `false`.
+
+This method can be much faster than `any(p, args)` or `any(args)` because its
+result may be determined at compile time.  However, `missing` values are not
+considered as special.
+
+To check whether predicate `p` returns `false` for all argument `args...` or
+whether all argument `args...` are false, repectively call:
+
+```julia
+noneof(p, args...) -> Bool
+```
+
+or
+
+```julia
+noneof(args...) -> Bool
+```
+
+which are the same as `!anyof(p, args...)` and `!anyof(args...)`.
+
+See also: [`any`](@ref), [`allof`](@ref).
+
+"""
+anyof(p::Function, a) = p(a)::Bool
+anyof(p::Function, a, b...) = p(a) || anyof(p, b...)
+anyof(a, b...) = anyof(a) || anyof(b...)
+anyof(a::Bool) = a
+function anyof(a::AbstractArray{Bool})
+    @inbounds for i in eachindex(a)
+        a[i] && return true
+    end
+    return false
+end
+function anyof(itr)
+    for val in itr
+        anyof(val) && return true
+    end
+    return false
+end
+
+noneof(args...) = ! anyof(args...)
+@doc @doc(anyof) noneof
+
+"""
+```julia
+reversemap(f, args)
+```
+
+applies the function `f` to arguments `args` in reverse order and return the
+result.  For now, the arguments `args` must be in the form of a simple tuple
+and the result is the tuple: `(f(args[end]),f(args[end-1]),...,f(args[1])`.
+
+Also see: [`map`](@ref), [`ntuple`](@ref).
+
+"""
+reversemap(f::Function, args::NTuple{N,Any}) where {N} =
+    ntuple(i -> f(args[(N + 1) - i]), Val(N))
+
+end # module
