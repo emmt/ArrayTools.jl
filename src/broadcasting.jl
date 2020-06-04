359592109aa5d30096da530f338324d857be0d9f
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2020-06-04 12:35:54 +0200

    Update doc.

--- src/broadcasting.jl
+++ src/broadcasting.jl
@@ -6,54 +6,51 @@
 
 """
 
-```julia
-bcastcopy(x, [T=eltype(x),] dims...)
-```
+    bcastcopy(A, [T=eltype(A),] dims...)
 
-yields a new array of element type `T` and dimensions `dims` filled with `x`
-according to type conversion and broadcasting rules (see [`broadcast`](@ref)).
-Compared to [`bcastlazy`](@ref), it is guaranteed that the returned array does
-not share its contents with `x`.
+yields a new array of element type `T` and dimensions `dims` whose values are
+given by `A` according to type conversion and broadcasting rules (like for the
+`broadcast` method).  Compared to [`bcastlazy`](@ref), it is guaranteed that
+the returned array does not share its contents with `A`.
 
-Argument `x` can be a scalar value or an array.
+Argument `A` can be a scalar value or an array.
 
-See also [`bcastlazy`](@ref), [`bcastdims`](@ref), [`reshape`](@ref).
+See also [`bcastlazy`](@ref), [`bcastdims`](@ref).
 
 """
-function bcastcopy(x, ::Type{T}, dims::Tuple{Vararg{Int}}) where {T}
-    A = Array{T}(undef, dims)
-    A .= x # This expression will clash if dimensions are not compatible.
-    return A
+function bcastcopy(A, ::Type{T}, dims::Tuple{Vararg{Int}}) where {T}
+    B = Array{T}(undef, dims)
+    B .= A # This expression will clash if dimensions are not compatible.
+    return B
 end
 
 # Convert dimensions.
-bcastcopy(x, ::Type{T}, dims::Tuple{Vararg{Integer}}) where {T} =
-    bcastcopy(x, T, map(Int, dims))
-bcastcopy(x, ::Type{T}, dims::Integer...) where {T} =
-    bcastcopy(x, T, dims)
+bcastcopy(A, ::Type{T}, dims::Tuple{Vararg{Integer}}) where {T} =
+    bcastcopy(A, T, map(Int, dims))
+bcastcopy(A, ::Type{T}, dims::Integer...) where {T} =
+    bcastcopy(A, T, dims)
 
 # Guess element type.
-bcastcopy(x, dims::Tuple{Vararg{Integer}}) =
-    bcastcopy(x, eltype(x), dims)
-bcastcopy(x, dims::Integer...) =
-    bcastcopy(x, dims)
+bcastcopy(A, dims::Tuple{Vararg{Integer}}) =
+    bcastcopy(A, eltype(A), dims)
+bcastcopy(A, dims::Integer...) =
+    bcastcopy(A, dims)
 
 """
 
-```julia
-bcastlazy(x, [T=eltype(x),] dims...)
-```
+    bcastlazy(A, [T=eltype(A),] dims...)
 
-yields a *flat* array of type `T` and dimensions `dims` filled with `x`
-according to type conversion and broadcasting rules (see [`broadcast`](@ref)).
-Compared to [`bcastcopy`](@ref), making a copy of `x` is avoided if it is
-already an array with the correct type of elements and dimensions or if it can
-be reshaped (see [`reshape`](@ref)) to the correct type and dimensions.  This
-means that the result may share the same contents as `x`.  Argument `x` can be
-a scalar or an array with 1-based indices.  The result has 1-based indices and
-contiguous elements which is suitable for fast linear indexing.
+yields a *flat* array of type `T` and dimensions `dims` whose values are given
+by `A` according to type conversion and broadcasting rules (see
+[`broadcast`](@ref)).  Compared to [`bcastcopy`](@ref), making a copy of `A` is
+avoided if it is already an array with the correct type of elements and
+dimensions or if it can be reshaped (by the `reshape` method) to the correct
+type and dimensions.  This means that the result may share the same contents as
+`A`.  Argument `A` can be a scalar or an array with 1-based indices.  The
+result has 1-based indices and contiguous elements which is suitable for fast
+linear indexing.
 
-See also [`bcastcopy`](@ref), [`bcastdims`](@ref), [`reshape`](@ref).
+See also [`bcastcopy`](@ref), [`bcastdims`](@ref).
 
 """
 function bcastlazy(A::DenseArray{T},
@@ -67,32 +64,30 @@ function bcastlazy(A::DenseArray{T},
     return (length(A) == prod(dims) ? reshape(A, dims) : bcastcopy(A, T, dims))
 end
 
-# Call `bcastcopy` if argument `x` can certainly not be returned.
-bcastlazy(x, ::Type{T}, dims::Tuple{Vararg{Int}}) where {T} =
-    bcastcopy(x, T, dims)
+# Call `bcastcopy` if argument `A` can certainly not be returned.
+bcastlazy(A, ::Type{T}, dims::Tuple{Vararg{Int}}) where {T} =
+    bcastcopy(A, T, dims)
 
 # Convert dimensions.
-bcastlazy(x, ::Type{T}, dims::Tuple{Vararg{Integer}}) where {T} =
-    bcastlazy(x, T, map(Int, dims))
-bcastlazy(x, ::Type{T}, dims::Integer...) where {T} =
-    bcastlazy(x, T, dims)
+bcastlazy(A, ::Type{T}, dims::Tuple{Vararg{Integer}}) where {T} =
+    bcastlazy(A, T, map(Int, dims))
+bcastlazy(A, ::Type{T}, dims::Integer...) where {T} =
+    bcastlazy(A, T, dims)
 
 # Guess element type.
-bcastlazy(x, dims::Tuple{Vararg{Integer}}) =
-    bcastlazy(x, eltype(x), dims)
-bcastlazy(x, dims::Integer...) =
-    bcastlazy(x, dims)
+bcastlazy(A, dims::Tuple{Vararg{Integer}}) =
+    bcastlazy(A, eltype(A), dims)
+bcastlazy(A, dims::Integer...) =
+    bcastlazy(A, dims)
 
 """
 
-```julia
-bcastdims(size(A), size(B), ...)
-```
+    bcastdims(size(A), size(B), ...) -> siz
 
-yields the dimensions of the array that would result from applying broadcasting
-rules (see [`broadcast`](@ref)) to arguments `A`, `B`, etc.  The result is a
-tuple of dimensions of type `Int`.  Call [`check_dimensions`](@ref) if you want
-to also make sure that the result is a list of valid dimensions.
+yields the size `siz` of the array that would result from applying broadcasting
+rules (see `broadcast`) to arguments `A`, `B`, etc.  The result is a tuple of
+integers (of type `Int`).  Call [`check_dimensions`](@ref) if you want to also
+make sure that the result is a list of valid dimensions.
 
 See also [`dimensions`](@ref), [`check_dimensions`](@ref), [`bcastcopy`](@ref),
 [`bcastlazy`](@ref).
@@ -111,8 +106,15 @@ bcastdims(a::Tuple{Vararg{Integer}}, ::Tuple{}) = bcastdims(a)
 bcastdims(a::Tuple{Vararg{Integer}}, b::Tuple{Vararg{Integer}}) =
     (bcastdim(a[1], b[1]), bcastdims(Base.tail(a), Base.tail(b))...)
 
-# Apply broadcasting rules for a single dimension (same as
-# Base.Broadcasting._bcs1 but takes care of converting to `Int`).
+"""
+    bcastdim(a, b) -> c
+
+applies broadcasting rules for a single dimension (same as
+`Base.Broadcasting._bcs1` but takes care of converting to `Int`), throwing an
+exception if dimensions `a` and `b` are not compatible according to these
+rules.
+
+"""
 bcastdim(a::Integer, b::Integer) = bcastdim(Int(a), Int(b))
 bcastdim(a::Int, b::Int) =
     (a == b || b == 1 ? a : a == 1 ? b :
